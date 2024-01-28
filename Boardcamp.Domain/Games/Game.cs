63ff354fc6c9abc299dab01bc1ed58296f24a3c8
Jefferson Araújo <jeffersonc201212@gmail.com>
2024-01-28 20:04:17 -0300

    FEAT: classe Games e Results Parttern criados.

---
+++ Boardcamp.Domain/Games/Game.cs
@@ -0,0 +1,55 @@
+﻿using Boardcamp.Domain.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boardcamp.Domain.Games
+{
+    public class Game
+    {
+        private Game(long id, string name, string image, int stock, decimal pricePerDay)
+        {
+            Id = id;
+            Name = name;
+            Image = image;
+            Stock = stock;
+            PricePerDay = pricePerDay;
+        }
+
+        protected Game() { }
+
+        public long Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Image { get; set; } = null!;
+        public int Stock { get; set; }
+        public decimal PricePerDay { get; set; }
+
+        public static Result<Game> Criar(long id, string name, string image, int stock, decimal pricePerDay)
+        {
+            if (id <= 0) return Result.Failure<Game>("O id deve ser maior ou igual que 0");
+            if (string.IsNullOrEmpty(name)) return Result.Failure<Game>("Você deve fornecer o nome do jogo");
+            if (string.IsNullOrEmpty(image)) return Result.Failure<Game>("Você deve fornecer o endereço da imagem do jogo");
+            if (stock < 0) return Result.Failure<Game>("Você não pode definir um estoque negativo");
+            if (pricePerDay <= 0) return Result.Failure<Game>("Você não pode definir um preço negativo ou nulo");
+
+            return Result.Success(new Game(id, name, image, stock, pricePerDay));
+        }
+
+        public Result Atualizar(string name, string image, int stock, decimal pricePerDay)
+        {
+            if (string.IsNullOrEmpty(name)) return Result.Failure("Você deve fornecer o nome do jogo");
+            if (string.IsNullOrEmpty(image)) return Result.Failure("Você deve fornecer o endereço da imagem do jogo");
+            if (stock < 0) return Result.Failure("Você não pode definir um estoque negativo");
+            if (pricePerDay <= 0) return Result.Failure("Você não pode definir um preço negativo ou nulo");
+
+            Name = name;
+            Image = image;
+            Stock = stock;
+            PricePerDay = pricePerDay;
+
+            return Result.Success();
+        }
+    }
+}
---
+++ Boardcamp.Domain/Results/Result.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boardcamp.Domain.Results
+{
+    public class Result<T>
+    {
+        private Result(){}
+
+        public bool IsSuccess { get; private set; }
+        public bool IsFailure => !IsSuccess;
+        public T? Value { get; private set; }
+        public string? ErrorMessage { get; private set; }
+
+        internal static Result<T> Success(T value)
+        {
+            return new Result<T> { IsSuccess = true, Value = value };
+        }
+
+        internal static Result<T> Failure(string errorMessage)
+        {
+            return new Result<T> { IsSuccess = false, ErrorMessage = errorMessage };
+        }
+    }
+
+    public class Result
+    {
+        private Result() { }
+
+        public bool IsSuccess { get; private set; }
+        public bool IsFailure => !IsSuccess;
+        public string? ErrorMessage { get; private set; }
+
+        public static Result<T> Success<T>(T value)
+        {
+            return Result<T>.Success(value);
+        }
+
+        public static Result Success()
+        {
+            return new Result { IsSuccess = true };
+        }
+
+        public static Result<T> Failure<T>(string errorMessage)
+        {
+            return Result<T>.Failure(errorMessage);
+        }
+
+        public static Result Failure(string errorMessage)
+        {
+            return new Result { IsSuccess = false, ErrorMessage = errorMessage };
+        }
+    }
+}
