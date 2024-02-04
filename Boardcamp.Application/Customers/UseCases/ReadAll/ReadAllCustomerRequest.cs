cc1b4b8b518a392f67e99369874bc81a6f720cd2
Jefferson Araújo <jeffersonc201212@gmail.com>
2024-02-04 16:06:16 -0300

    PARCIAL: progresso na criação do caso de uso de leitura.

---
+++ Boardcamp.Application/Customers/UseCases/ReadAll/ReadAllCustomerRequest.cs
@@ -0,0 +1,10 @@
+﻿using Boardcamp.Domain.Customers;
+using Boardcamp.Domain.Results;
+using MediatR;
+
+namespace Boardcamp.Application.Customers.UseCases.ReadAll
+{
+    public class ReadAllCustomerRequest : IRequest<Result<IEnumerable<Customer>>>
+    {
+    }
+}
