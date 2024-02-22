361a53d562b71cd6ef967a250e6b5aeff8e456ef
Jefferson Araújo <jeffersonc201212@gmail.com>
2024-02-22 16:01:01 -0300

    ADD: projeto infra.IoC adicionado.

---
+++ Boardcamp.Infra.IoC/DependencyInjection.cs
@@ -0,0 +1,35 @@
+﻿using Boardcamp.Domain.Customers.Repositories;
+using Boardcamp.Domain.Games.Repositories;
+using Boardcamp.Domain.Rentals.Repositories;
+using Boardcamp.Infra.Data.Contexts;
+using Boardcamp.Infra.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boardcamp.Infra.IoC
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(
+                    configuration.GetConnectionString("DevConnectionString"),
+                    b => b.MigrationsAssembly(typeof(AppDbContext).Assembly.FullName)
+                )
+            );
+
+            services.AddScoped<IGameRepository, GameRepository>();
+            services.AddScoped<ICustomerRepository, CustomerRepository>();
+            services.AddScoped<IRentalRepository, RentalRepository>();
+
+            return services;
+        }
+    }
+}
